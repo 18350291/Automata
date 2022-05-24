c5b5e70a6cf595964f88b213d3820da4b24721e9
Alex-2699 <alexantonio262699@gmail.com>
2022-05-24 15:11:31 -0500

    autómata 1010*

---
+++ src/automata/Automata.java
@@ -0,0 +1,56 @@
+
+package automata;
+
+public class Automata {
+    public static String cadenaOrig, cadenaAux;
+
+    public static void main(String[] args) {
+        Automata obj = new Automata();
+        cadenaOrig = "101000000";
+        cadenaAux = cadenaOrig;
+        
+        obj.Estado1(cadenaAux);
+        
+    }
+    
+    public void Estado1(String cadena){
+        Automata obj1 = new Automata();
+        
+        cadena = cadena.startsWith("101") ? cadena.substring(3) : cadena;
+        
+        if (cadenaAux.equals(cadena)){ // Se compara el valor de la cadena auxiliar y la cadena actual
+            //Si no sufrió cambios la cadena es inválida
+            obj1.Error(); //Se llama al método error y el proceso termina
+            
+        }else{
+            cadenaAux = cadena;
+            obj1.Estado2(cadena);
+        }
+    }
+    
+    public void Estado2(String cadena){
+        Automata obj2 = new Automata();
+        
+        cadenaAux = cadena;
+        
+        for (int i = 0; i <= cadenaAux.length()+1; i++){
+            cadena = cadena.startsWith("0") ? cadena.substring(1) : cadena;
+        }
+        
+        if (cadena.length()==0){
+            obj2.Validacion();
+        }else{
+            obj2.Error();
+        }
+                        
+    }
+    
+    public void Error(){
+        System.out.println("Cadena inválida");
+    }
+    
+    public void Validacion(){
+        System.out.println("Cadena válida");
+    }
+    
+}
